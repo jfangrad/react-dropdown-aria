b19fa5407a5d59713d7ae5fb650bc33b76078ecc
Justin Fangrad <justin.fangrad@gmail.com>
2018-07-10 22:08:04 -0400

    Add roll up to project

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,39 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: path.join(__dirname, "demo/src/index.jsx"),
+  entry: path.join(__dirname, 'demo/src/index.jsx'),
   output: {
-    path: path.join(__dirname, "demo/dist"),
-    filename: "bundle.js"
+    path: path.join(__dirname, 'demo/dist'),
+    filename: 'bundle.js',
   },
   module: {
     rules: [
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        use: ["babel-loader"]
+        use: ['babel-loader'],
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"]
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.scss$/,
-        use: ["style-loader", "css-loader", "sass-loader"]
-      }
-    ]
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
   },
   resolve: {
-    extensions: ['.js', '.jsx']
+    extensions: ['.js', '.jsx'],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, "demo/src/index.html"),
+      template: path.join(__dirname, 'demo/src/index.html'),
     }),
   ],
   devServer: {
-    contentBase: path.join(__dirname, "demo/dist"),
-    port: 8000
-  }
+    contentBase: path.join(__dirname, 'demo/dist'),
+    port: 8000,
+  },
 };
