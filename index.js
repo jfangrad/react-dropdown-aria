d23b5c80635136612536dc7a779a20ec09b71f71
Justin Fangrad <justin.fangrad@gmail.com>
2018-07-17 21:13:27 -0400

    Fix linting settings

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 if (process.env.NODE_ENV === 'production') {
   module.exports = require('./dist/react-aria-dropdown.min.js');
 } else {
-  module.exports = require('./dist/react-aria-dropdown.dev.js');
+  module.exports = require('./dist/react-aria-dropdown.dev.js'); // eslint-disable-line
 }
