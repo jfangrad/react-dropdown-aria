e62f5347366de1cbbd3b7845588f53668bfe4b79
Justin Fangrad <justin.fangrad@gmail.com>
2018-08-09 14:31:16 -0400

    More Tests. Enough for the little mean time

--- src/__tests__/Dropdown.test.js
+++ src/__tests__/Dropdown.test.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { shallow, mount } from 'enzyme';
+import sinon from 'sinon';
 import toJson from 'enzyme-to-json';
 import Dropdown from '../Dropdown';
-import { CUSTOM_OPTIONS } from './constants';
+import { CUSTOM_OPTIONS, OPTIONS } from './constants';
+import { KEY_CODES } from '../utils/helper';
 
 const foo = () => {};
 
@@ -32,19 +34,105 @@ describe('Check Props', () => {
 
 describe('Navigation', () => {
   let wrapper;
+  let button;
 
   beforeEach(() => {
-    wrapper = mount(<Dropdown options={CUSTOM_OPTIONS} setSelected={foo} />);
+    wrapper = mount(<Dropdown options={OPTIONS} setSelected={foo} />);
+    button = wrapper.find('.dropdown-select'); // Actual dropdown button element
   });
 
   it('Opens dropdown when clicked', () => {
-    wrapper.find('.dropdown-select').simulate('click');
+    button.simulate('click');
+
+    expect(wrapper.state('open')).toBeTruthy();
+  });
+
+  it('Opens dropdown with enter key', () => {
+    button.simulate('keyDown', { nativeEvent: { key: 'enter', keyCode: KEY_CODES.ENTER, preventDefault: foo } });
 
     expect(wrapper.state('open')).toBeTruthy();
   });
 
   it('Closes when clicked again', () => {
-    wrapper.find('.dropdown-select').simulate('click');
+    button.simulate('click');
+    expect(wrapper.state('open')).toBeTruthy();
+
+    button.simulate('click');
+    expect(wrapper.state('open')).toBeFalsy();
+  });
+
+  it('Closes when enter pressed again', () => {
+    button.simulate('keyDown', { nativeEvent: { key: 'enter', keyCode: KEY_CODES.ENTER, preventDefault: foo } });
+    expect(wrapper.state('open')).toBeTruthy();
+
+    button.simulate('keyDown', { nativeEvent: { key: 'enter', keyCode: KEY_CODES.ENTER, preventDefault: foo } });
+    expect(wrapper.state('open')).toBeFalsy();
+  });
+
+  it('Closes when tab key pressed', () => {
+    button.simulate('click');
+    expect(wrapper.state('open')).toBeTruthy();
+
+    button.simulate('keyDown', { nativeEvent: { key: 'tab', keyCode: KEY_CODES.TAB } });
+    expect(wrapper.state('open')).toBeFalsy();
+  });
+
+  it('Closes when esc pressed', () => {
+    button.simulate('click');
+    expect(wrapper.state('open')).toBeTruthy();
+
+    button.simulate('keyDown', { nativeEvent: { key: 'escape', keyCode: KEY_CODES.ESCAPE, preventDefault: foo } });
+    expect(wrapper.state('open')).toBeFalsy();
+  });
+
+  it('Arrow key selects first element in list', () => {
+    button.simulate('click');
+    wrapper.simulate('keyDown', { nativeEvent: { key: 'downArrow', keyCode: KEY_CODES.DOWN_ARROW, preventDefault: foo } });
+
+    expect(document.activeElement.innerHTML).toBe('1'); // eslint-disable-line no-undef
+  });
+
+  it('Arrow key nav loops arround', () => {
+    button.simulate('click');
+    for (let i = 0; i < 10; i += 1) {
+      wrapper.simulate('keyDown', { nativeEvent: { key: 'downArrow', keyCode: KEY_CODES.DOWN_ARROW, preventDefault: foo } });
+    }
+    expect(document.activeElement.innerHTML).toBe('10'); // eslint-disable-line no-undef
+
+    wrapper.simulate('keyDown', { nativeEvent: { key: 'downArrow', keyCode: KEY_CODES.DOWN_ARROW, preventDefault: foo } });
+    expect(document.activeElement.innerHTML).toBe('1'); // eslint-disable-line no-undef
+  });
+});
+
+describe('Selecting Options', () => {
+  let wrapper;
+  let button;
+  let spy;
+
+  beforeEach(() => {
+    spy = sinon.spy();
+    wrapper = mount(<Dropdown options={OPTIONS} setSelected={spy} />);
+    button = wrapper.find('.dropdown-select'); // Actual dropdown button element
+  });
+
+  it('calls setSelected with value when option selected', () => {
+    button.simulate('click');
+    wrapper.find('ul').childAt(0).simulate('click', { nativeEvent: { target: { innerText: '1' } } });
+
+    expect(spy.calledWith('1')).toBe(true);
+  });
+
+  it('sets dropdown text to selected item', () => {
+    button.simulate('click');
+    wrapper.find('ul').childAt(0).simulate('click', { nativeEvent: { target: { innerText: '1' } } });
+
+    expect(button.find('.displayed-value').text()).toBe('1');
+  });
+});
+
+describe('Special props', () => {
+  it('Does not open when disabled', () => {
+    const wrapper = mount(<Dropdown options={OPTIONS} setSelected={foo} disabled />);
     wrapper.find('.dropdown-select').simulate('click');
 
     expect(wrapper.state('open')).toBeFalsy();
