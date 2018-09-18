fc6b044223550d871b3200de9cb0023637ff03c1
Justin Fangrad <justin.fangrad@gmail.com>
2018-09-17 21:32:16 -0400

    Switch to using OptionItem component (#6)

--- src/utils/defaultOptionRenderer.jsx
+++ src/utils/defaultOptionRenderer.jsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import { cx } from 'emotion';
-// import OptionItem from '../components/OptionItem';
-
-/*
-This file needs changes once Enzyme updates to be able to handle forwardRef in tests
-*/
+import OptionItem from '../components/OptionItem';
 
 function defaultOptionRenderer(selectedOption, options, selectedOptionClassName, optionClassName, onOptionClicked, elementsRef, getStyle) {
   return options.map((option) => {
@@ -21,22 +17,6 @@ function defaultOptionRenderer(selectedOption, options, selectedOptionClassName,
             option.groupOptions.map((groupOption) => {
               const groupOptionClass = cx(groupOption.className, getStyle('optionItem', groupOption.value === selectedOption));
               return (
-                <button
-                  aria-label={groupOption.ariaLabel}
-                  className={groupOptionClass}
-                  onClick={onOptionClicked}
-                  onKeyDown={onOptionClicked}
-                  ref={el => el && elementsRef.push(el)}
-                  tabIndex="-1"
-                  title={groupOption.title}
-                  type="button"
-                  key={groupOption.value}
-                >
-                  { groupOption.iconClass && <i className={`${groupOption.iconClass} option-icon`} />}
-                  { groupOption.value }
-                </button>
-              );
-              /* return (
                 <OptionItem
                   key={groupOption.value}
                   optionClass={groupOptionClass}
@@ -44,39 +24,23 @@ function defaultOptionRenderer(selectedOption, options, selectedOptionClassName,
                   option={groupOption}
                   ref={el => el && elementsRef.push(el)}
                 />
-              ); */
+              );
             })
           }
         </div>
       );
     }
 
-    const optionClass = cx(className, getStyle('optionItem', value === selectedOption));
+    const optionClass = cx(className, getStyle('optionItem', { selected: value === selectedOption }));
     return (
-      <button
-        aria-label={option.ariaLabel}
-        className={optionClass}
-        onClick={onOptionClicked}
-        onKeyDown={onOptionClicked}
+      <OptionItem
+        key={value}
+        optionClass={optionClass}
+        onOptionClicked={onOptionClicked}
+        option={option}
         ref={el => el && elementsRef.push(el)}
-        tabIndex="-1"
-        title={option.title}
-        type="button"
-        key={option.value}
-      >
-        { option.iconClass && <i className={`${option.iconClass} option-icon`} />}
-        { option.value }
-      </button>
+      />
     );
-    // return (
-    //   <OptionItem
-    //     key={value}
-    //     optionClass={optionClass}
-    //     onOptionClicked={onOptionClicked}
-    //     option={option}
-    //     ref={el => el && elementsRef.push(el)}
-    //   />
-    // );
   });
 }
 
