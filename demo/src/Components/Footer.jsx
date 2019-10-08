8b1aafe6b08c18aaf8a77e376e46999c0f1e99cc
Justin Fangrad <justin.fangrad@gmail.com>
2019-10-08 09:18:16 -0400

    Add back in demo build for gh-pages

--- demo/src/Components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Footer = () => (
-  <div className="footer">
-    <div className="copyright">Copyright © Justin Fangrad, 2018. MIT Licensed.</div>
-  </div>
-);
-
-export default Footer;
--- packages/react-dropdown-aria/utils/helper.ts
+++
@@ -1,5 +0,0 @@
-import { Option, OptionGroup } from './types';
-
-export function isOptionGroup(option: Option | OptionGroup): option is OptionGroup {
-  return (option as OptionGroup).groupOptions !== undefined;
-}
