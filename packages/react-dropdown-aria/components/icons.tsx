f4da5f7a1dbcc9991291ea95766499cbe330b2a1
Justin Fangrad <justin.fangrad@gmail.com>
2020-05-15 11:54:15 -0400

    Fix icon sizes

--- packages/react-dropdown-aria/components/icons.tsx
+++ packages/react-dropdown-aria/components/icons.tsx
@@ -3,9 +3,10 @@ import React from 'react';
 interface StandardIconProps {
   children: React.ReactNode;
   className: string;
+  dim?: number;
 }
-const StandardIcon = ({ children, className }: StandardIconProps) => (
-  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className={`feather ${className}`}>
+const StandardIcon = ({ children, className, dim = 24 }: StandardIconProps) => (
+  <svg xmlns="http://www.w3.org/2000/svg" width={dim} height={dim} viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className={`feather ${className}`}>
     {children}
   </svg>
 )
@@ -18,13 +19,13 @@ export const Inbox = () => (
 );
 
 export const ChevronDown = () => (
-  <StandardIcon className="feather-chevron-down">
+  <StandardIcon className="feather-chevron-down" dim={16}>
     <polyline points="6 9 12 15 18 9" />
   </StandardIcon>
 );
 
 export const Search = () => (
-  <StandardIcon className="feather-search" >
+  <StandardIcon className="feather-search" dim={16}>
     <circle cx="11" cy="11" r="8" />
     <line x1="21" y1="21" x2="16.65" y2="16.65" />
   </StandardIcon>
